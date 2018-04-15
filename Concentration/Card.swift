40efb506cbf1f90c771eb3b38a02b24ad38e153a
Martin Rist <mlr-github@martinrist.com>
2018-04-15 12:28:53 +0100

    Complete lecture 2 demo

---
+++ Concentration/Card.swift
@@ -0,0 +1,26 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Martin Rist on 15/04/2018.
+//  Copyright © 2018 Martin Rist. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+
+    static var identiferFactory = 0
+
+    static func getUniqueIdentifier() -> Int {
+        identiferFactory += 1
+        return identiferFactory
+    }
+
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
---
+++ Concentration/Concentration/Concentration.swift
@@ -0,0 +1,44 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Martin Rist on 15/04/2018.
+//  Copyright © 2018 Martin Rist. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+
+    var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
+
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+
+            } else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: Shuffle cards
+    }
+}
