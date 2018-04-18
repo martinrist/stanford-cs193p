5ad9fe84866ce27373334806f7ba999f4da5a91a
Martin Rist <mlr-github@martinrist.com>
2018-04-18 16:13:52 +0100

    Add more tests for selecting and deselecting cards

--- Set/SetTests/SetTests.swift
+++ Set/SetTests/SetTests.swift
@@ -122,6 +122,56 @@ class SetTests: XCTestCase {
     XCTAssertFalse(game.board.contains(card5))
     XCTAssertFalse(game.board.contains(card6))
   }
+
+  func testSelectThenDeselectOneCard() {
+    var game = SetGame(boardSize: 6)
+    game.deal(numberOfCards: 1)
+    let card = game.board[0]!
+    game.select(card)
+    game.select(card)
+
+    XCTAssertEqual(game.selectedCards.count, 0)
+  }
+
+  func testSelectThenDeselectTwoCards() {
+    var game = SetGame(boardSize: 6)
+    game.deal(numberOfCards: 2)
+    let card1 = game.board[0]!
+    let card2 = game.board[1]!
+
+    game.select(card1)
+    game.select(card2)
+    XCTAssertEqual(game.selectedCards.count, 2)
+
+    game.select(card1)
+    XCTAssertEqual(game.selectedCards.count, 1)
+
+    game.select(card2)
+    XCTAssertEqual(game.selectedCards.count, 0)
+  }
+
+  func testSelectThenDeselectThreeCards() {
+    var game = SetGame(boardSize: 6)
+    game.deal(numberOfCards: 3)
+    let card1 = game.board[0]!
+    let card2 = game.board[1]!
+    let card3 = game.board[2]!
+
+    game.select(card1)
+    game.select(card2)
+    game.select(card3)
+    XCTAssertEqual(game.selectedCards.count, 3)
+
+    game.select(card1)
+    XCTAssertEqual(game.selectedCards.count, 2)
+
+    game.select(card2)
+    XCTAssertEqual(game.selectedCards.count, 1)
+
+    game.select(card3)
+    XCTAssertEqual(game.selectedCards.count, 0)
+
+  }
 }
 
 
